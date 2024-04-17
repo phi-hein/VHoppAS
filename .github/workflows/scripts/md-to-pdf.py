e5473d1a5fd70b723f5a16322fdcb1c7ef37fcce
Philipp Hein <phi-hein-dev@protonmail.com>
2024-04-17 16:21:34 +0200

    Test other md to html converter

--- .github/workflows/scripts/md-to-pdf.py
+++ .github/workflows/scripts/md-to-pdf.py
@@ -1,20 +1,31 @@
-import httpx
+# import httpx
+import gh_md_to_html
+# from xhtml2pdf import pisa
 
-with open("README.md") as mdfile:
-    body = mdfile.read()
+# Combine markdown files to a single string
+# with open("README.md") as mdfile:
+#     body = mdfile.read()
 
-response = httpx.post(
-    "https://api.github.com/markdown",
-    json={
-        "mode": "gfm",
-        "text": body
-    }
-)
+# Create table of contents
 
-print("GitHub API response code:",response.status_code)
-if response.status_code != 200:
-    print("Exit due to wrong API response.")
-    exit(1)
+# Convert to HTML using GitHub API
+# response = httpx.post(
+#     "https://api.github.com/markdown",
+#     json={
+#         "mode": "gfm",
+#         "text": body
+#     }
+# )
+# print("GitHub API response code:",response.status_code)
+# if response.status_code != 200:
+#     print("Exit due to wrong API response.")
+#     exit(1)
 
-with open(".github/workflows/scripts/VHoppAS-documentation.pdf",'w') as pdffile:
-    pdffile.write(response.text)
+gh_md_to_html.main("-t file -c gh-md-css ../USER_MANUAL.md")
+
+# Add surrounding html for pdf conversion
+# (also add that pdf was created using httpx and xhtml2pdf Python modules)
+
+
+# with open("VHoppAS-documentation.pdf",'w') as pdffile:
+#     pdffile.write(htmldoc)
